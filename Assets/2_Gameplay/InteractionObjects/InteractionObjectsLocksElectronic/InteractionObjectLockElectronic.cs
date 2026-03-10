f0d99153a534ca35f510ca7ded8c4834beaa3ac1
PanichMarkVladislavovich <panich.m.v@gmail.com>
2026-03-10 20:42:41 +0300

    Working on ElectronicLock puzzle

--- Assets/2_Gameplay/InteractionObjects/InteractionObjectsLocksElectronic/InteractionObjectLockElectronic.cs
+++ Assets/2_Gameplay/InteractionObjects/InteractionObjectsLocksElectronic/InteractionObjectLockElectronic.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.UI; // Подключаем пространство имен UI
+using UnityEngine.UI;
 
 public class InteractionObjectLockElectronic : MonoBehaviour, IInteractable
 {
-	public bool WasUnlocked {  get; private set; }
-
+	public bool WasUnlocked { get; private set; }
 	public string InteractionObjectNameSystem => throw new NotImplementedException();
-
 	public string InteractionObjectNameUI => "Электронный замок";
-
-	public string InteractionHintMessageMain => "Выскрыть?";
-
+	public string InteractionHintMessageMain => "Взломать?";
 	public string InteractionHintAction => throw new NotImplementedException();
-
 	public string InteractionHintMessageAdditional => throw new NotImplementedException();
 	private bool IsPuzzleActive;
 	public bool IsInteractionHintMessageAdditionalActive => throw new NotImplementedException();
@@ -23,14 +18,19 @@ public class InteractionObjectLockElectronic : MonoBehaviour, IInteractable
 	private Button buttonExitLockpickElectronicMenu;
 	private GameObject[] buttonsLockElectrical;
 	private MenuManager menuManager;
-    private GameObject canvasLockpickElectronicMenu;
+	private GameObject canvasLockpickElectronicMenu;
 	private SaveLoadController saveLoadController;
 	private GameSceneManager gameSceneManager;
 	private LocalizationManager localizationManager;
 
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	// Список индексов кнопок-"alarm"
+	private List<int> alarmIndices;
+
+	// Кол-во оставшихся ходов
+	private int movesLeft = 4;
+
 	void Start()
-    {
+	{
 		menuManager = ServiceLocator.Resolve<MenuManager>("MenuManager");
 		canvasLockpickElectronicMenu = ServiceLocator.Resolve<GameObject>("CanvasLockpickElectronicMenu");
 		buttonExitLockpickElectronicMenu = ServiceLocator.Resolve<Button>("ExitLockpickElectronic");
@@ -38,25 +38,24 @@ public class InteractionObjectLockElectronic : MonoBehaviour, IInteractable
 		gameSceneManager = ServiceLocator.Resolve<GameSceneManager>("GameSceneManager");
 		buttonsLockElectrical = ServiceLocator.Resolve<GameObject[]>("buttonsLockElectrical");
 
-		Debug.Log(buttonExitLockpickElectronicMenu);
-		buttonExitLockpickElectronicMenu.onClick.AddListener(OnClosePuzzle);// Присваиваем обработчик
+		// Добавляем обработчик закрытия головоломки при выходе
+		buttonExitLockpickElectronicMenu.onClick.AddListener(CloseElectronicLockPuzzle);
 
+		// Связываем управление панелями меню с открытием и закрытием паузы
 		menuManager.OnOpenPauseMenu += HidePuzzleCanvas;
 		menuManager.OnClosePauseMenu += ShowPuzzleCanvas;
-		gameSceneManager.OnBeginLoadMainMenuScene += OnClosePuzzle;
-		gameSceneManager.OnBeginLoadGameplayScene += OnClosePuzzle;
-		//buttonExitLockpickElectronicMenu.OnClick = () => CloseLockElectricalMenu();
+
+		// Закрываем головоломку при смене сцены
+		gameSceneManager.OnBeginLoadMainMenuScene += CloseElectronicLockPuzzle;
+		gameSceneManager.OnBeginLoadGameplayScene += CloseElectronicLockPuzzle;
 	}
 
-
-
-	private void OnClosePuzzle()
+	private void CloseElectronicLockPuzzle()
 	{
 		if (IsPuzzleActive)
 		{
 			IsPuzzleActive = false;
 			canvasLockpickElectronicMenu.SetActive(false);
-			gameObject.tag = "Interactable";
 			menuManager.CloseLockpickMenu();
 		}
 	}
@@ -79,12 +78,138 @@ public class InteractionObjectLockElectronic : MonoBehaviour, IInteractable
 
 	public void Interact()
 	{
-		menuManager.OpenLockpickMenu();
-		IsPuzzleActive = true;
-		//buttonExitLockpickElectronicMenu.onClick.RemoveAllListeners();      // Удаляем предыдущие события
-	
+		if (!IsPuzzleActive)
+		{
+			IsPuzzleActive = true;
+			menuManager.OpenLockpickMenu();
+			InitializeButtons();
+			ShowPuzzleCanvas();
+		}
+	}
+
+	private void InitializeButtons()
+	{
+		// Сброс количества ходов при перезапуске пазла
+		movesLeft = 4;
+
+		// Сначала очищаем состояние всех кнопок
+		foreach (var buttonObj in buttonsLockElectrical)
+		{
+			Button button = buttonObj.GetComponent<Button>();
+			ColorBlock colors = button.colors;
+			colors.normalColor = Color.white; // Восстанавливаем первоначальный цвет
+			button.colors = colors;
+			button.interactable = true;       // Активируем кнопку
+		}
+
+		// Список индексов кнопок
+		List<int> indices = Enumerable.Range(0, buttonsLockElectrical.Length).ToList();
+
+		// Случайно выбираем 4 кнопки из 9 и помечаем их как "alarm"
+		alarmIndices = new List<int>();
+		while (alarmIndices.Count < 4)
+		{
+			int index = UnityEngine.Random.Range(0, indices.Count);
+			alarmIndices.Add(indices[index]); // Запоминаем индексы alarm-кнопок
+			indices.RemoveAt(index);          // Исключаем выбранный индекс из дальнейшего выбора
+		}
+
+		// Добавляем обработчики нажатия на каждую кнопку
+		foreach (var buttonObj in buttonsLockElectrical)
+		{
+			Button button = buttonObj.GetComponent<Button>();
+			button.onClick.AddListener(() => OnButtonClicked(button));
+		}
+	}
+
+	private void OnButtonClicked(Button clickedButton)
+	{
+		// Определяем индекс нажатой кнопки
+		int buttonIndex = Array.IndexOf(buttonsLockElectrical, clickedButton.gameObject);
+
+		// Проверяем, является ли данная кнопка "alarm"
+		if (alarmIndices.Contains(buttonIndex)) // Если нажали на alarm-кнопку
+		{
+			// Проигрыш: красим все кнопки в красный
+			Debug.Log("FAIL!");
+			SetAllButtonsRed();
+		
+			return;
+		}
+
+		// Если кнопка нормальная: красим её в зелёный и делаем неактивной
+		ColorBlock colors = clickedButton.colors;
+		colors.normalColor = Color.green;
+		clickedButton.colors = colors;
+		clickedButton.interactable = false;
+
+		// Раскрываем одну из случайных alarm-кнопок
+		RevealAlarmButton();
+
+		// Уменьшаем счётчик ходов
+		movesLeft--;
+
+		// Завершаем пазл, если закончились ходы
+		if (movesLeft <= 0)
+		{
+			EndPuzzle();
+		}
+	}
+
+	private void SetAllButtonsRed()
+	{
+		Debug.Log("SET ALL RED");
+		foreach (var buttonObj in buttonsLockElectrical)
+		{
+			Button button = buttonObj.GetComponent<Button>();
+			ColorBlock colors = button.colors;
+			colors.normalColor = Color.red;
+			button.colors = colors;
+			button.interactable = false;
+		}
+		CloseElectronicLockPuzzle();
+	}
+
+	private void RevealAlarmButton()
+	{
+		// Получаем список доступных alarm-кнопок
+		List<Button> availableAlarms = GetAvailableAlarmButtons();
+
+		if (availableAlarms.Any())
+		{
+			// Выбираем случайную из доступных alarm-кнопок
+			int randomIndex = UnityEngine.Random.Range(0, availableAlarms.Count);
+			Button revealedButton = availableAlarms[randomIndex];
+
+			// Красим выбранную alarm-кнопку в красный и отключаем
+			ColorBlock colors = revealedButton.colors;
+			colors.normalColor = Color.red;
+			revealedButton.colors = colors;
+			revealedButton.interactable = false;
+		}
+	}
+
+	private void EndPuzzle()
+	{
+		WasUnlocked = true;
+		IsPuzzleActive = false;
+		HidePuzzleCanvas();
+		menuManager.CloseLockpickMenu();
+	}
 
 
-		ShowPuzzleCanvas();
+
+	private List<Button> GetAvailableAlarmButtons()
+	{
+		List<Button> result = new List<Button>();
+		foreach (int index in alarmIndices)
+		{
+			Button button = buttonsLockElectrical[index].GetComponent<Button>();
+			if (button.interactable) // Только активные кнопки включаем в выбор
+			{
+				result.Add(button);
+			}
+		}
+		return result;
 	}
 }
