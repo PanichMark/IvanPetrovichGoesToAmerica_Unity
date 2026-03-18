0142753e092288abdee98fb94aced3f229321ec9
PanichMarkVladislavovich <panich.m.v@gmail.com>
2026-03-18 20:24:47 +0300

    Started working on TV logic

---
+++ Assets/2_Gameplay/InteractionObjects/InteractionObjectsTV/InteractionObjectTVChangeChannel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class InteractionObjectTVButtonChannel : MonoBehaviour, IInteractable
+{
+	[SerializeField] private string buttonChannelUI;
+	public string InteractionObjectNameSystem => "buttonChannel";
+	
+	public string InteractionObjectNameUI => buttonChannelUI;
+
+	public string InteractionHintMessageMain => $"Нажать {buttonChannelUI}?";
+	[SerializeField] bool IsNextChannel;
+	public string InteractionHintAction => throw new System.NotImplementedException();
+
+	public string InteractionHintMessageAdditional => throw new System.NotImplementedException();
+
+	public bool IsInteractionHintMessageAdditionalActive => false;
+
+	public void Interact()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/2_Gameplay/InteractionObjects/InteractionObjectsTV/InteractionObjectTVPowerButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class InteractionObjectTVPowerButton : MonoBehaviour, IInteractable
+{
+	public string InteractionObjectNameSystem => throw new System.NotImplementedException();
+	private string PowerButtonName = "Кнопка питания телевизора";
+	public string InteractionObjectNameUI => PowerButtonName;
+
+	public string InteractionHintMessageMain => $"Нажать {PowerButtonName}?";
+	[SerializeField] GameObject TVscreen;
+	public string InteractionHintAction => throw new System.NotImplementedException();
+	private bool IsTVturnedOn;
+	public string InteractionHintMessageAdditional => throw new System.NotImplementedException();
+
+	public bool IsInteractionHintMessageAdditionalActive => false;
+
+	public void Interact()
+	{
+		if (IsTVturnedOn)
+		{
+			TVscreen.SetActive(false);
+			IsTVturnedOn = false;
+		}
+		else
+		{
+			TVscreen.SetActive(true);
+			IsTVturnedOn = true;
+		}
+	}
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+		TVscreen.SetActive(false);
+		IsTVturnedOn = false;
+    }
+
+ 
+}
