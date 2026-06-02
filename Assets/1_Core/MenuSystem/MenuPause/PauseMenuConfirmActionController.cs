7f6d44dd0f2f98011d7c7775167af9f94a44a8d0
PanichMarkVladislavovich <panich.m.v@gmail.com>
2026-06-02 14:31:25 +0300

    Finished Localizing PauseConfirmAction menu

--- Assets/1_Core/MenuSystem/MenuPause/PauseMenuConfirmActionController.cs
+++ Assets/1_Core/MenuSystem/MenuPause/PauseMenuConfirmActionController.cs
@@ -132,7 +132,7 @@ public class PauseMenuConfirmActionController : MonoBehaviour
 
 	private void HandleShowForNewSaveFile(int slot)
 	{
-		_textComponentActionMessage.text = $"{_textConfirmCreateNewGameFile}";
+		_textComponentActionMessage.text = $"{_textConfirmCreateNewGameFile}?";
 
 		_actionOnAccept = () => StartCoroutine(_saveLoadController.SaveGame(slot));
 
@@ -170,7 +170,7 @@ public class PauseMenuConfirmActionController : MonoBehaviour
 	{
 		_menuManager.OpenConfirmationOnExitToMainMenu();
 
-		_textComponentActionMessage.text = $"{_textConfirmExitToMainMenu}";
+		_textComponentActionMessage.text = $"{_textConfirmExitToMainMenu}?";
 
 		_actionOnAccept = () => 
 		{
@@ -183,42 +183,42 @@ public class PauseMenuConfirmActionController : MonoBehaviour
 
 	private void HandleShowForSaveSettingsGeneral()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsGeneral}";
+		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsGeneral}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionGeneralController.SaveSettingsGeneral();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
 
 	private void HandleShowForResetSettingsGeneral()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsGeneral}";
+		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsGeneral}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionGeneralController.ResetSettingsGeneral();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
 
 	private void HandleShowForSaveSettingsControls()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsControls}";
+		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsControls}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionControlsController.SaveSettingsControls();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
 
 	private void HandleShowForResetSettingsControls()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsControls}";
+		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsControls}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionControlsController.ResetSettingsControls();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
 
 	private void HandleShowForSaveSettingsGraphics()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsGraphics}";
+		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsGraphics}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionGraphicsController.SaveSettingsGraphics();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
 
 	private void HandleShowForResetSettingsGraphics()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsGraphics}";
+		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsGraphics}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionGraphicsController.ResetSettingsGraphics();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
@@ -226,14 +226,14 @@ public class PauseMenuConfirmActionController : MonoBehaviour
 
 	private void HandleShowForSaveSettingsAudio()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsAudio}";
+		_textComponentActionMessage.text = $"{_textConfirmSaveSettings} {_textSettingsAudio}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionAudioController.SaveSettingsAudio();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
 
 	private void HandleShowForResetSettingsAudio()
 	{
-		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsAudio}";
+		_textComponentActionMessage.text = $"{_textConfirmResetSettings} {_textSettingsAudio}?";
 		_actionOnAccept = () => _pauseSubMenuSettingsSectionAudioController.ResetSettingsAudio();
 		_pauseMenuController.OpenPauseConfirmMenu();
 	}
@@ -260,22 +260,22 @@ public class PauseMenuConfirmActionController : MonoBehaviour
 	{
 		_localizationManager = localizationManager;
 
-		_textConfirmCreateNewGameFile = _localizationManager.GetLocalizedString("");
-		_textConfirmRewriteGameFile = _localizationManager.GetLocalizedString("");
-		_textConfirmDeleteGameFile = _localizationManager.GetLocalizedString("");
-		_textConfirmLoadGameFile = _localizationManager.GetLocalizedString("");
+		_textConfirmCreateNewGameFile = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextConfirmCreateNewGameFile");
+		_textConfirmRewriteGameFile = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextConfirmRewriteGameFile");
+		_textConfirmDeleteGameFile = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextConfirmDeleteGameFile");
+		_textConfirmLoadGameFile = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextConfirmLoadGameFile");
 
-		_textConfirmExitToMainMenu = _localizationManager.GetLocalizedString("");
+		_textConfirmExitToMainMenu = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextConfirmExitToMainMenu");
 	
-		_textSettingsGeneral = _localizationManager.GetLocalizedString("");
-		_textSettingsControls = _localizationManager.GetLocalizedString("");
-		_textSettingsGraphics = _localizationManager.GetLocalizedString("");
-		_textSettingsAudio = _localizationManager.GetLocalizedString("");
+		_textSettingsGeneral = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextSettingsGeneral");
+		_textSettingsControls = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextSettingsControls");
+		_textSettingsGraphics = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextSettingsGraphics");
+		_textSettingsAudio = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextSettingsAudio");
 
-		_textConfirmSaveSettings = _localizationManager.GetLocalizedString("");
-		_textConfirmResetSettings = _localizationManager.GetLocalizedString("");
+		_textConfirmSaveSettings = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextSaveSettings");
+		_textConfirmResetSettings = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_TextResetSettings");
 
-		_textButtonComponentConfirmAction.text = _localizationManager.GetLocalizedString("");
-		_textButtonComponentCancelAction.text = _localizationManager.GetLocalizedString("");
+		_textButtonComponentConfirmAction.text = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_ButtonConfirmAction");
+		_textButtonComponentCancelAction.text = _localizationManager.GetLocalizedString("UI_Menu_PauseConfirmActionMenu_ButtonCancelAction");
 	}
 }
