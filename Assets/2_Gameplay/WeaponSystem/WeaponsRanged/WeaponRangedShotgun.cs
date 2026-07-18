89f3b850d566752e9071b9b15123754ab93738e0
PanichMarkVladislavovich <panich.m.v@gmail.com>
2026-07-19 02:01:56 +0300

    Working on Shotgun Reloading animation and logic

--- Assets/2_Gameplay/WeaponSystem/WeaponsRanged/WeaponRangedShotgun.cs
+++ Assets/2_Gameplay/WeaponSystem/WeaponsRanged/WeaponRangedShotgun.cs
@@ -7,12 +7,18 @@ public class WeaponRangedShotgun : WeaponRangedAbstract
 	public override WeaponTypes WeaponType => WeaponTypes.Ranged;
 	public override AmmoTypes PlayerWeaponAmmoType => AmmoTypes.Ammo12gauge;
 	public override float WeaponDamage => 100f;
-	protected override float _waitForAmmoRefill => 1;
+	protected override float _waitForAmmoRefill => 3.125f;
 	public override bool IsWeaponAuto => false;
 	public override bool IsReloadingAnimationSingle => true;
 	public override float WeaponAttackSpeedRate => 0.15f;
+
+	private GameObject _shotgunBarrel1stPerson;
+	private GameObject _shotgunBarrel3rdPerson;
+
 	protected override void InitializeWeaponRanged()
 	{
+		_shotgunBarrel1stPerson = FirstPersonWeaponModelInstance.transform.Find("Barrel").gameObject;
+		_shotgunBarrel3rdPerson = ThirdPersonWeaponModelInstance.transform.Find("Barrel").gameObject;
 
 		_VFXshottEffect = Resources.Load<GameObject>($"VFXs/VFX_MuzzleFlash");
 	}
@@ -87,5 +93,104 @@ public class WeaponRangedShotgun : WeaponRangedAbstract
 	protected override void ApplyWeaponRecoil()
 	{
 		_playerCameraController.ApplyWeaponRecoilSingle(15, 0.05f, 0.5f);
+	}
+
+	public override IEnumerator ReloadWeaponPlayer(bool isSecondAnimation)
+	{
+		int ammoToAdd = Mathf.Min(PlayerAmmoReserve, PlayerMagazineAmmoMax - PlayerMagazineAmmoCurrent);
+		var data = _playerResourcesAmmoManager.AmmoDictionary[PlayerWeaponAmmoType];
+
+		Coroutine animRoutine = StartCoroutine(_weaponAnimationController.PrepareForReloadingWeapon(this, IsReloadingAnimationSingle, isSecondAnimation));
+
+		StartCoroutine(ShotgunReloadBreakActionOpen());
+
+		yield return new WaitForSeconds(_waitForAmmoRefill);
+
+		data.AmmoReserve -= ammoToAdd;
+		_playerResourcesAmmoManager.AmmoDictionary[PlayerWeaponAmmoType] = data;
+		PlayerMagazineAmmoCurrent += ammoToAdd;
+
+		if (System.Enum.TryParse(WeaponName.ToString(), out WeaponNames parsedWeaponType))
+		{
+			_playerResourcesAmmoManager.NotifyReserveAmmoChanged(PlayerWeaponAmmoType, data.AmmoReserve);
+			_playerResourcesAmmoManager.NotifyMagazineAmmoChanged(parsedWeaponType, PlayerWeaponAmmoType, PlayerMagazineAmmoCurrent);
+		}
+
+		StartCoroutine(ShotgunReloadBreakActionClose());
+
+		yield return animRoutine;
+
+		Debug.Log("Reloaded");
+		yield return null;
+	}
+
+	private IEnumerator ShotgunReloadBreakActionOpen()
+	{
+		yield return new WaitForSeconds(1.250f);
+
+		float duration = 0.167f;
+		float targetAngle = 75f; // Угол поворота по локальной оси X
+
+		Transform barrel1stTrans = _shotgunBarrel1stPerson.transform;
+		Transform barrel3rdTrans = _shotgunBarrel3rdPerson.transform;
+
+		// Сохраняем начальные повороты
+		Quaternion startRot1st = barrel1stTrans.localRotation;
+		Quaternion startRot3rd = barrel3rdTrans.localRotation;
+
+		// Вычисляем целевой поворот
+		Quaternion endRot1st = startRot1st * Quaternion.Euler(targetAngle, 0f, 0f);
+		Quaternion endRot3rd = startRot3rd * Quaternion.Euler(targetAngle, 0f, 0f);
+
+		float timer = 0f;
+
+		while (timer < duration)
+		{
+			float t = Mathf.Clamp01(timer / duration);
+
+			// Обращаемся к .transform.localRotation
+			barrel1stTrans.localRotation = Quaternion.Lerp(startRot1st, endRot1st, t);
+			barrel3rdTrans.localRotation = Quaternion.Lerp(startRot3rd, endRot3rd, t);
+
+			timer += Time.deltaTime;
+			yield return null;
+		}
+
+		barrel1stTrans.localRotation = endRot1st;
+		barrel3rdTrans.localRotation = endRot3rd;
+	}
+
+	private IEnumerator ShotgunReloadBreakActionClose()
+	{
+		yield return new WaitForSeconds(0.417f);
+
+		float duration = 0.208f;
+		float targetAngle = -75f; // Изменено на отрицательное значение
+
+		Transform barrel1stTrans = _shotgunBarrel1stPerson.transform;
+		Transform barrel3rdTrans = _shotgunBarrel3rdPerson.transform;
+
+		Quaternion startRot1st = barrel1stTrans.localRotation;
+		Quaternion startRot3rd = barrel3rdTrans.localRotation;
+
+		// Целевой поворот будет "текущий угол МИНУС 75"
+		Quaternion endRot1st = startRot1st * Quaternion.Euler(targetAngle, 0f, 0f);
+		Quaternion endRot3rd = startRot3rd * Quaternion.Euler(targetAngle, 0f, 0f);
+
+		float timer = 0f;
+
+		while (timer < duration)
+		{
+			float t = Mathf.Clamp01(timer / duration);
+
+			barrel1stTrans.localRotation = Quaternion.Lerp(startRot1st, endRot1st, t);
+			barrel3rdTrans.localRotation = Quaternion.Lerp(startRot3rd, endRot3rd, t);
+
+			timer += Time.deltaTime;
+			yield return null;
+		}
+
+		barrel1stTrans.localRotation = endRot1st;
+		barrel3rdTrans.localRotation = endRot3rd;
 	}
 }
