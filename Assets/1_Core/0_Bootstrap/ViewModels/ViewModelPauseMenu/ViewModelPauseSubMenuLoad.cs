9f85a735959d9b5c37525c133e4e5553bb387bbf
PanichMarkVladislavovich <panich.m.v@gmail.com>
2026-07-31 17:37:55 +0300

    Working on Updating Save/Load UI

--- Assets/1_Core/0_Bootstrap/ViewModels/ViewModelPauseMenu/ViewModelPauseSubMenuLoad.cs
+++ Assets/1_Core/0_Bootstrap/ViewModels/ViewModelPauseMenu/ViewModelPauseSubMenuLoad.cs
@@ -1,32 +1,46 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class ViewModelPauseSubMenuLoad
 {
+	private int _numberOfSlots = 5;
+
 	public GameObject TextPauseSubMenuLoad;
 
-	public GameObject[] ButtonsLoadGameFile = new GameObject[5];
-	public GameObject[] TextGameFileDateAndTime = new GameObject[5];
-	public GameObject[] TextGameFileSceneName = new GameObject[5];
-	public GameObject[] ImageSceneGameFile = new GameObject[5];
+	public GameObject[] ButtonsLoadGameFile;
+	public GameObject[] TextGameFileMissionName;
+	public GameObject[] TextGameFileSceneName;
+	public GameObject[] TextGameFileDateAndTime;
+	public GameObject[] ImageSceneGameFile;
+	public TextMeshProUGUI[] TextGameFileSlotNumber;
 
 	public GameObject ButtonClosePauseSubMenuLoad;
 	public GameObject TextButtonClosePauseSubMenuLoad;
 
 	public ViewModelPauseSubMenuLoad(Bootstrap bootstrap, GameObject canvas)
 	{
+		ButtonsLoadGameFile = new GameObject[_numberOfSlots];
+		TextGameFileMissionName = new GameObject[_numberOfSlots];
+		TextGameFileSceneName = new GameObject[_numberOfSlots];
+		TextGameFileDateAndTime = new GameObject[_numberOfSlots];
+		ImageSceneGameFile = new GameObject[_numberOfSlots];
+
+		TextGameFileSlotNumber = new TextMeshProUGUI[_numberOfSlots];
+
 		TextPauseSubMenuLoad = bootstrap.FindDeepGameObject(canvas, "TextPauseSubMenuLoad");
 
 		for (int i = 0; i < 5; i++)
 		{
-			string buttonLoadGameFile = "ButtonLoadGameFile" + (i + 1);
-			string textDateAndTimeGameFile = "TextDateAndTimeGameFile" + (i + 1);
-			string textSceneNameGameFile = "TextSceneNameGameFile" + (i + 1);
-			string imageSceneGameFile = "ImageSceneGameFile" + (i + 1);
+			string slotRootName = "ButtonLoadGameFile" + (i + 1);
 
-			ButtonsLoadGameFile[i] = bootstrap.FindDeepGameObject(canvas, buttonLoadGameFile);
-			TextGameFileDateAndTime[i] = bootstrap.FindDeepGameObject(canvas, textDateAndTimeGameFile);
-			TextGameFileSceneName[i] = bootstrap.FindDeepGameObject(canvas, textSceneNameGameFile);
-			ImageSceneGameFile[i] = bootstrap.FindDeepGameObject(canvas, imageSceneGameFile);
+			ButtonsLoadGameFile[i] = bootstrap.FindDeepGameObject(canvas, $"{slotRootName}");
+			Debug.Log(ButtonsLoadGameFile[i]);
+			TextGameFileMissionName[i] = bootstrap.FindDeepGameObject(ButtonsLoadGameFile[i], "TextMissionName");
+			TextGameFileSceneName[i] = bootstrap.FindDeepGameObject(ButtonsLoadGameFile[i], "TextSceneName");
+			TextGameFileDateAndTime[i] = bootstrap.FindDeepGameObject(ButtonsLoadGameFile[i], "TextDateAndTime");
+			ImageSceneGameFile[i] = bootstrap.FindDeepGameObject(ButtonsLoadGameFile[i], "ImageSceneGameFile");
+			TextGameFileSlotNumber[i] = bootstrap.FindDeepGameObject(ButtonsLoadGameFile[i], "TextSlotNumber").GetComponent<TextMeshProUGUI>();
+			TextGameFileSlotNumber[i].text = $"{i + 1}";
 		}
 
 		ButtonClosePauseSubMenuLoad = bootstrap.FindDeepGameObject(canvas, "ButtonClosePauseSubMenuLoad");
