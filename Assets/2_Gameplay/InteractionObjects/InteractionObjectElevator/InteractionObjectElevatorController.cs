f9da64cda987202b896a27c5b279150f316622b2
PanichMarkVladislavovich <panich.m.v@gmail.com>
2026-08-25 19:34:23 +0300

    Working on Elevator Logic

--- Assets/2_Gameplay/InteractionObjects/InteractionObjectElevator/InteractionObjectElevatorController.cs
+++ Assets/2_Gameplay/InteractionObjects/InteractionObjectElevator/InteractionObjectElevatorController.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class InteractionObjectElevatorController : GameplayObjectSaveLoad
 {
-	[SerializeField] private float _elevatorSpeed = 2.0f;
-	[SerializeField] private float _elevatorUpPosition = 5.0f;
-	[SerializeField] private float _elevatorPositionTolerance = 0.1f;
+	[SerializeField] private float _elevatorHeightOffset;
+	[SerializeField] private float _elevatorSpeed;
+	private float _elevatorPositionTolerance = 0.01f;
 
 	[SerializeField] private InteractionObjectElevatorButton _downCallButton;
 	[SerializeField] private InteractionObjectElevatorButton _downSendButton;
 	[SerializeField] private InteractionObjectElevatorButton _upCallButton;
 	[SerializeField] private InteractionObjectElevatorButton _upSendButton;
 
-	[SerializeField] private float _doorSlidingPosition;
+	[SerializeField] private float _doorOpenPosition;
 	[SerializeField] private float _doorSlidingSpeed;
 	[SerializeField] private GameObject _cabinDoorRight;
 	[SerializeField] private GameObject _cabinDoorLeft;
@@ -22,7 +22,10 @@ public class InteractionObjectElevatorController : GameplayObjectSaveLoad
 	[SerializeField] private GameObject _upDoorLeft;
 
 	private float _elevatorDownPosition;
-	private bool _isElevatorMoving = false;
+	private float _elevatorUpPosition;
+	private bool _isElevatorMoving;
+
+	private bool _areDoorsPresent;
 
 	void Start()
 	{
@@ -31,8 +34,58 @@ public class InteractionObjectElevatorController : GameplayObjectSaveLoad
 		_downCallButton.Initialize(this, false, false);
 		_downSendButton.Initialize(this, false, true);
 		_upCallButton.Initialize(this, true, true);
-
 		_upSendButton.Initialize(this, true, false);
+
+		_elevatorDownPosition = transform.position.y;
+		_elevatorUpPosition = _elevatorDownPosition + _elevatorHeightOffset;
+
+		_areDoorsPresent = (_cabinDoorRight != null && _cabinDoorLeft != null && _downDoorRight != null && _downDoorLeft != null && _upDoorRight != null && _upDoorLeft != null);
+
+		ImmediatelyOpenFloorDoors(false);
+	}
+
+	private void ImmediatelyOpenFloorDoors(bool isTopFloor)
+	{
+		if (_areDoorsPresent)
+		{
+			OpenDoor(_cabinDoorRight, true);
+			OpenDoor(_cabinDoorLeft, false);
+
+			if (!isTopFloor)
+			{
+				OpenDoor(_downDoorRight, true);
+				OpenDoor(_downDoorLeft, false);
+			}
+			else
+			{
+				OpenDoor(_upDoorRight, true);
+				OpenDoor(_upDoorLeft, false);
+			}
+		}
+	}
+
+	private void OpenDoor(GameObject door, bool isDoorRight)
+	{
+		if (isDoorRight)
+		{
+			door.transform.localPosition = new Vector3(door.transform.localPosition.x - _doorOpenPosition, door.transform.localPosition.y, door.transform.localPosition.z);
+		}
+		else
+		{
+			door.transform.localPosition = new Vector3(door.transform.localPosition.x + _doorOpenPosition, door.transform.localPosition.y, door.transform.localPosition.z);
+		}
+	}
+
+	private void CloseDoor(GameObject door, bool isDoorRight)
+	{
+		if (isDoorRight)
+		{
+			door.transform.localPosition = new Vector3(door.transform.localPosition.x + _doorOpenPosition, door.transform.localPosition.y, door.transform.localPosition.z);
+		}
+		else
+		{
+			door.transform.localPosition = new Vector3(door.transform.localPosition.x - _doorOpenPosition, door.transform.localPosition.y, door.transform.localPosition.z);
+		}
 	}
 
 	public bool MoveElevator(bool moveUp)
@@ -52,6 +105,7 @@ public class InteractionObjectElevatorController : GameplayObjectSaveLoad
 	private IEnumerator ElevatorAnimation(float targetY)
 	{
 		_isElevatorMoving = true;
+	
 		Vector3 targetPosition = new Vector3(transform.position.x, targetY, transform.position.z);
 
 		while (Vector3.Distance(transform.position, targetPosition) > _elevatorPositionTolerance)
